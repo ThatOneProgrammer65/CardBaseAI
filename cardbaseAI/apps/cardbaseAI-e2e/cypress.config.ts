54146c4a136837099ca737476925fa0bc6aa0ae8
Nate <nathan.emmrich@outlook.com>
2025-10-21 13:28:21 -0400

    initial commit

---
+++ cardbaseAI/apps/cardbaseAI-e2e/cypress.config.ts
@@ -0,0 +1,17 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: {
+        default: 'npx nx run @cardbase-ai/cardbaseAI:serve',
+        production: 'npx nx run @cardbase-ai/cardbaseAI:serve',
+      },
+      ciWebServerCommand: 'npx nx run @cardbase-ai/cardbaseAI:serve',
+      ciBaseUrl: 'http://localhost:4200',
+    }),
+    baseUrl: 'http://localhost:8081',
+  },
+});
